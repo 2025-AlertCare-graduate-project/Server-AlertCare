b13f20cb0f706c4fa9c31f35d1c0779146ac98d2
mxinseo <kyungminseo10@gmail.com>
2025-06-25 23:27:46 +0900

    Fix : 회원가입/로그인 response message 수정

--- alertcare/src/main/java/com/alertcare/server/user/controller/UserController.java
+++ alertcare/src/main/java/com/alertcare/server/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.alertcare.server.user.controller;
 import com.alertcare.server.common.response.BasicResponse;
 import com.alertcare.server.user.domain.User;
 import com.alertcare.server.user.dto.LoginDTO;
+import com.alertcare.server.user.dto.ProfileRequestDTO;
 import com.alertcare.server.user.dto.SignUpDTO;
 import com.alertcare.server.user.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +21,7 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public BasicResponse<User> signUp(@RequestBody SignUpDTO signUpDTO) {
         User createdUser = userService.signUp(signUpDTO);
-        return BasicResponse.success(201, "유저 생성 성공", createdUser);
+        return BasicResponse.success(201, "회원가입 성공", createdUser);
     }
 
     @PostMapping("/login")
@@ -28,6 +29,6 @@ public class UserController {
     public BasicResponse<User> login(@RequestBody LoginDTO loginDTO) {
         User user = userService.login(loginDTO);
 
-        return  BasicResponse.success(200, "유저 조회 성공", user);
+        return BasicResponse.success(200, "로그인 성공", user);
     }
 }
