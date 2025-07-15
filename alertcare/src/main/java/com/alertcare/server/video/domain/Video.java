b3a37ded36a082641beb43502aa7e415c053a26a
ljinny <104649081+ljinny@users.noreply.github.com>
2025-07-15 22:47:31 +0900

    Feat: 영상 전송 api 연결 구현  #12

---
+++ alertcare/src/main/java/com/alertcare/server/video/domain/Video.java
@@ -0,0 +1,34 @@
+package com.alertcare.server.video.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Video {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String videoUrl;
+
+    private boolean fallDetected;
+
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Video(String videoUrl, boolean fallDetected, LocalDateTime createdAt){
+        this.videoUrl = videoUrl;
+        this.fallDetected = fallDetected;
+        this.createdAt = createdAt;
+    }
+}
