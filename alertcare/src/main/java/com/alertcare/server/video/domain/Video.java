62cf8c222e10c210fe4d1b7a2b6ecbc4bf90fc8e
mxinseo <kyungminseo10@gmail.com>
2025-07-30 22:25:31 +0900

    Fix : Video 병합된 영상을 저장하도록 수정

--- alertcare/src/main/java/com/alertcare/server/video/domain/Video.java
+++ alertcare/src/main/java/com/alertcare/server/video/domain/Video.java
@@ -1,9 +1,8 @@
 package com.alertcare.server.video.domain;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.alertcare.server.user.domain.User;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,22 +12,28 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 @NoArgsConstructor
+@Table(name = "Video")
 public class Video {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String videoUrl;
+    private String fallDetectVideoUrl;
 
-    private boolean fallDetected;
+    private LocalDateTime fallDetectTime;
 
-    private LocalDateTime createdAt;
+    private boolean isVideoAccessible;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Builder
-    public Video(String videoUrl, boolean fallDetected, LocalDateTime createdAt){
-        this.videoUrl = videoUrl;
-        this.fallDetected = fallDetected;
-        this.createdAt = createdAt;
+    public Video(String fallDetectVideoUrl, LocalDateTime fallDetectTime, boolean isVideoAccessible, User user) {
+        this.fallDetectVideoUrl = fallDetectVideoUrl;
+        this.fallDetectTime = fallDetectTime;
+        this.isVideoAccessible = true;
+        this.user = user;
     }
 }
--- alertcare/src/main/java/com/alertcare/server/video/service/VideoService.java
+++ alertcare/src/main/java/com/alertcare/server/video/service/VideoService.java
@@ -1,26 +1,54 @@
 package com.alertcare.server.video.service;
 
+import com.alertcare.server.user.domain.User;
+import com.alertcare.server.user.exception.UserErrorCode;
+import com.alertcare.server.user.exception.UserException;
+import com.alertcare.server.user.repository.UserRepository;
 import com.alertcare.server.video.domain.Video;
+import com.alertcare.server.video.dto.VideoListResponseDto;
 import com.alertcare.server.video.dto.VideoRequestDto;
 import com.alertcare.server.video.repository.VideoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class VideoService {
 
     private final VideoRepository videoRepository;
+    private final UserRepository userRepository;
 
-    public void saveVideo(VideoRequestDto dto){
+    public void saveVideo(VideoRequestDto dto) {
+        User user = userRepository.findByCareReceiverPhoneNumber(dto.getCareReceiverPhoneNumber().trim())
+                .orElseThrow(() -> new UserException(UserErrorCode.MEMBER_NOT_FOUND));
+
         Video video = Video.builder()
-                .videoUrl(dto.getVideoUrl())
-                .fallDetected(dto.isFallDetected())
-                .createdAt(LocalDateTime.now())
+                .fallDetectVideoUrl(dto.getVideoUrl())
+                .user(user)
+                .fallDetectTime(LocalDateTime.now())
                 .build();
 
         videoRepository.save(video);
+    }
+
+    public List<VideoListResponseDto> getVideoList() {
+        List<Video> videoList = videoRepository.findAll();
+
+        return videoList.stream()
+                .map(video -> {
+                    // 48시간 지나면 만료
+                    boolean isExpired = video.getFallDetectTime().plusHours(48).isBefore(LocalDateTime.now());
+
+                    return new VideoListResponseDto(
+                            video.getFallDetectTime(),
+                            video.getFallDetectVideoUrl(),
+                            isExpired
+                    );
+                })
+                .collect(Collectors.toList());
     }
 }
