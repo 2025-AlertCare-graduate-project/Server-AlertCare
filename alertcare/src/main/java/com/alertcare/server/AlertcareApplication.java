655e3298d868d3c461a40c67c3ed9188db74d65e
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-30 23:08:14 +0900

    [mod/#15] 기타 설정 추가

--- alertcare/src/main/java/com/alertcare/server/AlertcareApplication.java
+++ alertcare/src/main/java/com/alertcare/server/AlertcareApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlertcareApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AlertcareApplication.class, args);
-    }
 
+        SpringApplication.run(AlertcareApplication.class, args);
+
+
+    }
 }
