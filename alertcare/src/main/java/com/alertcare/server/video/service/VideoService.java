93ab462f2e4eef8707e32428220758cc807f4747
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-30 22:59:47 +0900

    [mod/#15] VideoList 함수 제거

--- alertcare/src/main/java/com/alertcare/server/video/service/VideoService.java
+++ alertcare/src/main/java/com/alertcare/server/video/service/VideoService.java
@@ -5,15 +5,12 @@ import com.alertcare.server.user.exception.UserErrorCode;
 import com.alertcare.server.user.exception.UserException;
 import com.alertcare.server.user.repository.UserRepository;
 import com.alertcare.server.video.domain.Video;
-import com.alertcare.server.video.dto.VideoListResponseDto;
 import com.alertcare.server.video.dto.VideoRequestDto;
 import com.alertcare.server.video.repository.VideoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -35,20 +32,4 @@ public class VideoService {
         videoRepository.save(video);
     }
 
-    public List<VideoListResponseDto> getVideoList() {
-        List<Video> videoList = videoRepository.findAll();
-
-        return videoList.stream()
-                .map(video -> {
-                    // 48시간 지나면 만료
-                    boolean isExpired = video.getFallDetectTime().plusHours(48).isBefore(LocalDateTime.now());
-
-                    return new VideoListResponseDto(
-                            video.getFallDetectTime(),
-                            video.getFallDetectVideoUrl(),
-                            isExpired
-                    );
-                })
-                .collect(Collectors.toList());
-    }
 }
