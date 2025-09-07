e1dd9656008975d8c9ac64bb2bf88f21e9086e5f
ljinny <104649081+ljinny@users.noreply.github.com>
2025-09-08 00:24:11 +0900

    "Feat: 하루 합계 저장 스케줄 코드 구현 #46"

--- alertcare/src/main/java/com/alertcare/server/summary/repository/SummaryRepository.java
+++ alertcare/src/main/java/com/alertcare/server/summary/repository/SummaryRepository.java
@@ -1,12 +1,7 @@
 package com.alertcare.server.summary.repository;
 
 import com.alertcare.server.summary.domain.Summary;
-import com.alertcare.server.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.time.LocalDate;
 
 public interface SummaryRepository extends JpaRepository<Summary, Long> {
 
--- alertcare/src/main/java/com/alertcare/server/summary/service/SummaryService.java
+++ alertcare/src/main/java/com/alertcare/server/summary/service/SummaryService.java
@@ -1,4 +1,56 @@
 package com.alertcare.server.summary.service;
 
+import com.alertcare.server.activity.repository.ActivityRepository;
+import com.alertcare.server.summary.domain.Summary;
+import com.alertcare.server.summary.repository.SummaryRepository;
+import com.alertcare.server.user.domain.User;
+import com.alertcare.server.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class SummaryService {
+
+    private final SummaryRepository summaryRepository;
+    private final ActivityRepository activityRepository;
+    private final UserRepository userRepository;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    @Transactional
+    public void saveDailySummary(){
+        LocalDate date = LocalDate.now().minusDays(1);
+        LocalDateTime startOfDay = date.atStartOfDay();
+        LocalDateTime endOfDay = date.atTime(23, 59, 59);
+
+        List<User> users = userRepository.findAll();
+
+        for(User user : users) {
+            Object[] result = (Object[]) activityRepository.sumByDateAndUser(user.getId(), startOfDay, endOfDay);
+
+            int activeSum = ((Number) result[0]).intValue();
+            int sittingSum = ((Number) result[1]).intValue();
+            int lyingSum = ((Number) result[2]).intValue();
+
+            Summary summary = Summary.builder()
+                    .user(user)
+                    .date(date)
+                    .activeTime(activeSum)
+                    .sittingTime(sittingSum)
+                    .lyingTime(lyingSum)
+                    .build();
+
+            summaryRepository.save(summary);
+        }
+
+        System.out.println("Daily summary finished for date: " + date);
+
+    }
+
 }
