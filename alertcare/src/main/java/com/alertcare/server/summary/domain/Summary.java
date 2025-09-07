3a88b7afefe0336d5c5a14ba1a37abc9d4eb3736
ljinny <104649081+ljinny@users.noreply.github.com>
2025-09-07 16:56:27 +0900

    Feat: 하루 요약 저장 엔티티 생성

---
+++ alertcare/src/main/java/com/alertcare/server/summary/domain/Summary.java
@@ -0,0 +1,32 @@
+package com.alertcare.server.summary.domain;
+
+import com.alertcare.server.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "summary")
+public class Summary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    private int activeTime;
+    private int sittingTime;
+    private int lyingTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
