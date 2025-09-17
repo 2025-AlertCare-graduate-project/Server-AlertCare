d7ec244811b9bd9f6cb4a650542a405c2bce2f24
ljinny <104649081+ljinny@users.noreply.github.com>
2025-09-17 20:48:57 +0900

    "Feat: gpt 요청용 dto 구성 #49"

---
+++ alertcare/src/main/java/com/alertcare/server/openai/dto/OpenAiResponse.java
@@ -0,0 +1,24 @@
+package com.alertcare.server.openai.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OpenAiResponse {
+    private List<Choice> choices;
+
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Choice {
+        private int index;
+        private Message message;
+
+    }
+}
